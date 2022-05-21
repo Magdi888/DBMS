30b37cb3fd57fe0ae6b822c809ab3f28a8f1681c
Ahmed <a.magdi888@gmail.com>
2022-05-21 22:40:42 +0200

    Update

--- TableBaseFunc.sh
+++ TableBaseFunc.sh
@@ -192,38 +192,50 @@ function UpdateTable {
     then
         if [ -f $1 ]
         then
-			read -p "Enter the Column Name : " ColName
+			read -p "Enter the Column Name Condition : " ColName
 			exist=$(awk  -F : -v c=$ColName '{if($1 == c)print $0 }' $1.meta)
 			if ! [[ $exist == "" ]]
 			then
 				(( num=$(awk  -F : -v c=$ColName '{if($1 == c)print NR }' $1.meta) -1 ))
 				 read -p "Enter the Value Condition : " Val
 				 in=$(awk  -F : -v c=$Val '{if($'$num' == c)print $0 }' $1)
-				 if ! [[ $in == "" ]]
-				 then
+				 echo $in
+				 where=$(awk -F : -v c=$Val '{if($'$num' == c)print NR }' $1)
+				 echo $where
+				if ! [[ $in == "" ]]
+				then
 					 read -p "Enter Column Name to Set: " Cond
 					 exist=$(awk  -F : -v c=$Cond '{if($1 == c)print $0 }' $1.meta)
 					 type=$(awk  -F : -v c=$Cond '{if($1 == c)print $2 }' $1.meta)
-					 if ! [[ $exist == "" ]]
-					 then
-					 (( num1=$(awk  -F : -v c=$Cond '{if($1 == c)print NR }' $1.meta) -1 ))
-					 	read -p "Enter the New Value Condition : " NewVal
-						 if [[ $type == "Integer"]]
-						 then
-						 	if ! [[ $NewVal =~ ^[0-9]+$]]
-							 then
+					if ! [[ $exist == "" ]]
+					then
+						(( num1=$(awk  -F : -v c=$Cond '{if($1 == c)print NR }' $1.meta) -1 ))
+						echo $num1
+						echo $where
+						old=$(awk -F : -v c=$where '{if(NR == c) print $'$num1'}' $1)
+						echo $old
+					 	read -p "Enter the New Value : " NewVal
+						if [[ $type == "Integer" ]]
+						then
+						 	if ! [[ $NewVal =~ ^[0-9]+$ ]]
+							then
 							 	echo "Invailed Input"
-							 fi
-						 elif [[ $type == "Sting" ]]
-						 then
+							fi
+						elif [[ $type == "Sting" ]]
+						then
 						 	if ! [[ $NewVal =~ ^[a-zA-Z]+$ ]]
-							 then
+							then
 							 	echo "Invailed Input"
-				 			 fi
-						 else
-						 fi
-
+				 			fi
+						fi
+						 sed -i ''$where's/'$old'/'$NewVal'/g' $1
+						 echo "update success"
+					else
+					 	echo "Target column does not exist"
 				 	fi
+				else 
+				 	echo "The value condition does not exist"
+				fi
 			else
 				echo "column not found"
 			fi
